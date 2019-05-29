9f42a92c290f56b9b9ad773d97b264037505c767
Kate Pronina <kate-pronina-97@yandex.ru>
2019-05-29 16:55:02 +0300

    Created another test and release choosing the right type of slider

--- src/js/View/view.test.ts
+++ src/js/View/view.test.ts
@@ -1,8 +1,15 @@
 import View from './view';
 import RangeSliderView from './Views/rangeSliderView';
+import IntervalSliderView from './Views/intervalSliderView';
 
 test('Should create right slider view', (): void => {
-    const view = new View({parentId: 'bar', type: 'range'});
+  const view = new View({parentId: 'bar', type: 'range'});
 
-    expect(view.slider).toEqual(expect.any(RangeSliderView));
+  expect(view.slider).toEqual(expect.any(RangeSliderView));
+})
+
+test('Should create right slider view for interval', (): void => {
+  const view = new View({parentId: 'bar', type: 'interval'});
+
+  expect(view.slider).toEqual(expect.any(IntervalSliderView));
 })
--- src/js/View/view.ts
+++ src/js/View/view.ts
@@ -1,15 +1,18 @@
 import Settings from '../application.interfaces';
 import RangeSliderView from './Views/rangeSliderView';
+import IntervalSliderView from './Views/intervalSliderView';
 
 class View {
   public state: Settings;
-  public slider: RangeSliderView;
+  public slider: RangeSliderView | IntervalSliderView;
 
   public constructor(state: Settings) {
     this.state = state;
 
     if (this.state.type === 'range') {
       this.slider = new RangeSliderView(this.state);
+    } else if (this.state.type === 'interval') {
+      this.slider = new IntervalSliderView(this.state);
     }
   }
 }
