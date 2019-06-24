b59d6842d0c67a98ea698b53b71ca66a49166656
Kate Pronina <kate-pronina-97@yandex.ru>
2019-06-24 15:42:19 +0300

    Grouped tests

--- src/js/Model/model.test.ts
+++ src/js/Model/model.test.ts
@@ -1,65 +1,69 @@
 import Model from './model';
-import {FullSettings, Settings} from '../application.interfaces';
 
-test('Should set default settings', (): void => {
-  const model = new Model({parentId: 'foo'});
+describe('Constructor', (): void => {
+  test('Should set default settings', (): void => {
+    const model = new Model({parentId: 'foo'});
 
-  expect(model.state.parentId).toEqual('foo');
-  expect(model.state.type).toEqual('range');
-  expect(model.state.minValue).toEqual(0);
-  expect(model.state.maxValue).toEqual(100);
-  expect(model.state.value).toEqual(model.state.minValue);
-  expect(model.state.step).toEqual(1);
-  expect(model.state.direction).toEqual('horizontal');
-  expect(model.state.hint).toEqual(true);
-  expect(model.state.scale).toEqual(false);
-  expect(model.state.configure).toEqual(false);
-})
+    expect(model.state.parentId).toEqual('foo');
+    expect(model.state.type).toEqual('range');
+    expect(model.state.minValue).toEqual(0);
+    expect(model.state.maxValue).toEqual(100);
+    expect(model.state.value).toEqual(model.state.minValue);
+    expect(model.state.step).toEqual(1);
+    expect(model.state.direction).toEqual('horizontal');
+    expect(model.state.hint).toEqual(true);
+    expect(model.state.scale).toEqual(false);
+    expect(model.state.configure).toEqual(false);
+  })
 
-test('Should set custom settings', (): void => {
-  const model = new Model({
-    parentId: 'bar',
-    type: 'interval',
-    minValue: 5,
-    maxValue: 30,
-    step: 5,
-    direction: 'vertical',
-    hint: false,
-    scale: true,
-    configure: true,
-  });
+  test('Should set custom settings', (): void => {
+    const model = new Model({
+      parentId: 'bar',
+      type: 'interval',
+      minValue: 5,
+      maxValue: 30,
+      step: 5,
+      direction: 'vertical',
+      hint: false,
+      scale: true,
+      configure: true,
+    });
 
-  expect(model.state.parentId).toEqual('bar');
-  expect(model.state.type).toEqual('interval');
-  expect(model.state.minValue).toEqual(5);
-  expect(model.state.maxValue).toEqual(30);
-  expect(model.state.step).toEqual(5);
-  expect(model.state.direction).toEqual('vertical');
-  expect(model.state.hint).toEqual(false);
-  expect(model.state.scale).toEqual(true);
-  expect(model.state.configure).toEqual(true);
-})
+    expect(model.state.parentId).toEqual('bar');
+    expect(model.state.type).toEqual('interval');
+    expect(model.state.minValue).toEqual(5);
+    expect(model.state.maxValue).toEqual(30);
+    expect(model.state.step).toEqual(5);
+    expect(model.state.direction).toEqual('vertical');
+    expect(model.state.hint).toEqual(false);
+    expect(model.state.scale).toEqual(true);
+    expect(model.state.configure).toEqual(true);
+  })
+});
 
-test('Should update value', (): void => {
-  const model = new Model({parentId: 'foo'});
+describe('Setting value', (): void => {
+  test('Should update value', (): void => {
+    const model = new Model({parentId: 'foo'});
 
-  model.setValue(10);
+    model.setValue(10);
 
-  expect(model.state.value).toEqual(10);
-})
+    expect(model.state.value).toEqual(10);
+  })
 
-test('Should not update wrong value', (): void => {
-  const model = new Model({parentId: 'foo'});
+  test('Should not update wrong value', (): void => {
+    const model = new Model({parentId: 'foo'});
 
-  model.setValue(10000);
+    model.setValue(10000);
 
-  expect(model.state.value).toEqual(100);
-})
+    expect(model.state.value).toEqual(100);
+  })
 
-test('Should not update wrong value', (): void => {
-  const model = new Model({parentId: 'foo'});
+  test('Should not update wrong value', (): void => {
+    const model = new Model({parentId: 'foo'});
 
-  model.setValue(-10000);
+    model.setValue(-10000);
 
-  expect(model.state.value).toEqual(0);
-})
+    expect(model.state.value).toEqual(0);
+  })
+});
+
--- src/js/View/view.test.ts
+++ src/js/View/view.test.ts
@@ -5,213 +5,218 @@ import HintView from './Views/hintView';
 import ScaleView from './Views/scaleView';
 import ConfigureView from './Views/configureView';
 
-test('Should create right slider view', (): void => {
-  document.body.innerHTML = '<div id="bar"></div>';
+describe('Constructor', (): void => {
+  test('Should create right slider view', (): void => {
+    document.body.innerHTML = '<div id="bar"></div>';
 
-  const view = new View({parentId: 'bar', 
-                        type: 'range',
-                        minValue: 0,
-                        maxValue: 100,
-                        value: 0,
-                        step: 1,
-                        direction: 'horizontal',
-                        hint: true,
-                        scale: false,
-                        configure: false,
-                      });
+    const view = new View({parentId: 'bar', 
+                          type: 'range',
+                          minValue: 0,
+                          maxValue: 100,
+                          value: 0,
+                          step: 1,
+                          direction: 'horizontal',
+                          hint: true,
+                          scale: false,
+                          configure: false,
+                        });
 
-  expect(view.slider).toEqual(expect.any(RangeSliderView));
-})
+    expect(view.slider).toEqual(expect.any(RangeSliderView));
+  })
 
-// TO DO: add getDOMElement to IntervalSLider view
+  // TO DO: add getDOMElement to IntervalSLider view
 
-// test('Should create right slider view for interval', (): void => {
-//   document.body.innerHTML = '<div id="bar"></div>';
+  // test('Should create right slider view for interval', (): void => {
+  //   document.body.innerHTML = '<div id="bar"></div>';
 
-//   const view = new View({parentId: 'bar', 
-//                         type: 'interval',
-//                         minValue: 0,
-//                         maxValue: 100,
-//                         value: 0,
-//                         step: 1,
-//                         direction: 'horizontal',
-//                         hint: 'yes',
-//                         scale: 'no',
-//                         configure: 'no',
-//                       });
+  //   const view = new View({parentId: 'bar', 
+  //                         type: 'interval',
+  //                         minValue: 0,
+  //                         maxValue: 100,
+  //                         value: 0,
+  //                         step: 1,
+  //                         direction: 'horizontal',
+  //                         hint: 'yes',
+  //                         scale: 'no',
+  //                         configure: 'no',
+  //                       });
 
-//   expect(view.slider).toEqual(expect.any(IntervalSliderView));
-// })
+  //   expect(view.slider).toEqual(expect.any(IntervalSliderView));
+  // })
 
-test('Should create hint', (): void => {
-  const view = new View({parentId: 'bar',
-                        type: 'range',
-                        minValue: 0,
-                        maxValue: 100,
-                        value: 0,
-                        step: 1,
-                        direction: 'horizontal',
-                        hint: true,
-                        scale: false,
-                        configure: false,
-                      });
+  test('Should create hint', (): void => {
+    const view = new View({parentId: 'bar',
+                          type: 'range',
+                          minValue: 0,
+                          maxValue: 100,
+                          value: 0,
+                          step: 1,
+                          direction: 'horizontal',
+                          hint: true,
+                          scale: false,
+                          configure: false,
+                        });
 
-  expect(view.hint).toEqual(expect.any(HintView));
-})
+    expect(view.hint).toEqual(expect.any(HintView));
+  })
 
-test('Should not create hint', (): void => {
-  const view = new View({parentId: 'bar',
-                        type: 'range',
-                        minValue: 0,
-                        maxValue: 100,
-                        value: 0,
-                        step: 1,
-                        direction: 'horizontal',
-                        hint: false,
-                        scale: false,
-                        configure: false,
-                      });
+  test('Should not create hint', (): void => {
+    const view = new View({parentId: 'bar',
+                          type: 'range',
+                          minValue: 0,
+                          maxValue: 100,
+                          value: 0,
+                          step: 1,
+                          direction: 'horizontal',
+                          hint: false,
+                          scale: false,
+                          configure: false,
+                        });
 
-  expect(view.hint).toBeUndefined();
-})
+    expect(view.hint).toBeUndefined();
+  })
 
-test('Should create scale', (): void => {
-  const view = new View({parentId: 'bar',
-                        type: 'range',
-                        minValue: 0,
-                        maxValue: 100,
-                        value: 0,
-                        step: 1,
-                        direction: 'horizontal',
-                        hint: true,
-                        scale: true,
-                        configure: false,
-                      });
+  test('Should create scale', (): void => {
+    const view = new View({parentId: 'bar',
+                          type: 'range',
+                          minValue: 0,
+                          maxValue: 100,
+                          value: 0,
+                          step: 1,
+                          direction: 'horizontal',
+                          hint: true,
+                          scale: true,
+                          configure: false,
+                        });
 
-  expect(view.scale).toEqual(expect.any(ScaleView));
-})
+    expect(view.scale).toEqual(expect.any(ScaleView));
+  })
 
-test('Should not create scale', (): void => {
-  const view = new View({parentId: 'bar',
-                        type: 'range',
-                        minValue: 0,
-                        maxValue: 100,
-                        value: 0,
-                        step: 1,
-                        direction: 'horizontal',
-                        hint: true,
-                        scale: false,
-                        configure: false,
-                      });
+  test('Should not create scale', (): void => {
+    const view = new View({parentId: 'bar',
+                          type: 'range',
+                          minValue: 0,
+                          maxValue: 100,
+                          value: 0,
+                          step: 1,
+                          direction: 'horizontal',
+                          hint: true,
+                          scale: false,
+                          configure: false,
+                        });
 
-  expect(view.scale).toBeUndefined();
-})
+    expect(view.scale).toBeUndefined();
+  })
 
-test('Should create configuration panel', (): void => {;
-  const view = new View({parentId: 'bar',
-                        type: 'range',
-                        minValue: 0,
-                        maxValue: 100,
-                        value: 0,
-                        step: 1,
-                        direction: 'horizontal',
-                        hint: true,
-                        scale: false,
-                        configure: true,
-                      });
+  test('Should create configuration panel', (): void => {;
+    const view = new View({parentId: 'bar',
+                          type: 'range',
+                          minValue: 0,
+                          maxValue: 100,
+                          value: 0,
+                          step: 1,
+                          direction: 'horizontal',
+                          hint: true,
+                          scale: false,
+                          configure: true,
+                        });
 
-  expect(view.configure).toEqual(expect.any(ConfigureView));
-})
+    expect(view.configure).toEqual(expect.any(ConfigureView));
+  })
 
-test('Should not create configuration panel', (): void => {
-  const view = new View({parentId: 'bar',
-                        type: 'range',
-                        minValue: 0,
-                        maxValue: 100,
-                        value: 0,
-                        step: 1,
-                        direction: 'horizontal',
-                        hint: true,
-                        scale: false,
-                        configure: false,
-                      });
+  test('Should not create configuration panel', (): void => {
+    const view = new View({parentId: 'bar',
+                          type: 'range',
+                          minValue: 0,
+                          maxValue: 100,
+                          value: 0,
+                          step: 1,
+                          direction: 'horizontal',
+                          hint: true,
+                          scale: false,
+                          configure: false,
+                        });
 
-  expect(view.configure).toBeUndefined();
-})
+    expect(view.configure).toBeUndefined();
+  })
 
-test('Should save parent element', (): void => {
-  document.body.innerHTML = '<div id="bar"></div>';
+  test('Should save parent element', (): void => {
+    document.body.innerHTML = '<div id="bar"></div>';
 
-  const view = new View({parentId: 'bar',
-                        type: 'range',
-                        minValue: 0,
-                        maxValue: 100,
-                        value: 0,
-                        step: 1,
-                        direction: 'horizontal',
-                        hint: true,
-                        scale: false,
-                        configure: false,
-                      });
+    const view = new View({parentId: 'bar',
+                          type: 'range',
+                          minValue: 0,
+                          maxValue: 100,
+                          value: 0,
+                          step: 1,
+                          direction: 'horizontal',
+                          hint: true,
+                          scale: false,
+                          configure: false,
+                        });
 
-  expect(view.parentElement).toBeDefined();
-  expect(view.parentElement).toEqual(expect.anything());
-})
+    expect(view.parentElement).toBeDefined();
+    expect(view.parentElement).toEqual(expect.anything());
+  })
+});
 
-test('Should add slider in DOM', (): void => {
-  document.body.innerHTML = '<div id="first-slider"></div>';
+describe('DOM actions', (): void => {
+  test('Should add slider in DOM', (): void => {
+    document.body.innerHTML = '<div id="first-slider"></div>';
 
-  const view = new View({parentId: 'first-slider',
-                        type: 'range',
-                        minValue: 0,
-                        maxValue: 100,
-                        value: 0,
-                        step: 1,
-                        direction: 'horizontal',
-                        hint: true,
-                        scale: false,
-                        configure: false,
-                      });
+    const view = new View({parentId: 'first-slider',
+                          type: 'range',
+                          minValue: 0,
+                          maxValue: 100,
+                          value: 0,
+                          step: 1,
+                          direction: 'horizontal',
+                          hint: true,
+                          scale: false,
+                          configure: false,
+                        });
 
-  expect(document.querySelectorAll('#first-slider .slider-wrapper').length).toEqual(1);
-  expect(document.querySelectorAll('#first-slider .slider').length).toEqual(1);
-  expect(document.querySelectorAll('#first-slider .slider__bar').length).toEqual(1);
-  expect(document.querySelectorAll('#first-slider .slider__point').length).toEqual(1);
-})
+    expect(document.querySelectorAll('#first-slider .slider-wrapper').length).toEqual(1);
+    expect(document.querySelectorAll('#first-slider .slider').length).toEqual(1);
+    expect(document.querySelectorAll('#first-slider .slider__bar').length).toEqual(1);
+    expect(document.querySelectorAll('#first-slider .slider__point').length).toEqual(1);
+  })
 
-test('Should add hint in DOM with correct value', (): void => {
-  document.body.innerHTML = '<div id="foo"></div>';
+  test('Should add hint in DOM with correct value', (): void => {
+    document.body.innerHTML = '<div id="foo"></div>';
 
-  const view = new View({parentId: 'foo', 
-                        type: 'range',
-                        minValue: 0,
-                        maxValue: 100,
-                        value: 0,
-                        step: 1,
-                        direction: 'horizontal',
-                        hint: true,
-                        scale: false,
-                        configure: false,
-                      });
+    const view = new View({parentId: 'foo', 
+                          type: 'range',
+                          minValue: 0,
+                          maxValue: 100,
+                          value: 0,
+                          step: 1,
+                          direction: 'horizontal',
+                          hint: true,
+                          scale: false,
+                          configure: false,
+                        });
 
-  expect(document.querySelectorAll('#foo .slider__hint').length).toEqual(1);
-  expect((document.querySelector('#foo .slider__hint') as HTMLElement).textContent).toEqual('0');
-})
+    expect(document.querySelectorAll('#foo .slider__hint').length).toEqual(1);
+    expect((document.querySelector('#foo .slider__hint') as HTMLElement).textContent).toEqual('0');
+  })
 
-test('Should not add hint in DOM', (): void => {
-  document.body.innerHTML = '<div id="foo"></div>';
+  test('Should not add hint in DOM', (): void => {
+    document.body.innerHTML = '<div id="foo"></div>';
 
-  const view = new View({parentId: 'foo',
-                        type: 'range',
-                        minValue: 0,
-                        maxValue: 100,
-                        value: 0,
-                        step: 1,
-                        direction: 'horizontal',
-                        hint: false,
-                        scale: false,
-                        configure: false,
-                      });
+    const view = new View({parentId: 'foo',
+                          type: 'range',
+                          minValue: 0,
+                          maxValue: 100,
+                          value: 0,
+                          step: 1,
+                          direction: 'horizontal',
+                          hint: false,
+                          scale: false,
+                          configure: false,
+                        });
 
-  expect(document.querySelectorAll('#foo .slider__hint').length).toEqual(0);
-})
+    expect(document.querySelectorAll('#foo .slider__hint').length).toEqual(0);
+  })
+});
+
