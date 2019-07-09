5fcb2994ad6e13ca61a7f44e667918e3858b7931
Kate Pronina <kate-pronina-97@yandex.ru>
2019-07-09 23:39:31 +0300

    Edited demo page

--- src/js/demo.js
+++ src/js/demo.js
@@ -6,7 +6,7 @@ import Application from './application';
   };
 })(jQuery);
 
-$('#first-slider').slider({value: 67, configuration: true, step: 10});
-$('#second-slider').slider({minValue: 3, maxValue: 19, step: 5, value: 8, configuration: true});
+$('#first-slider').slider({configuration: true});
+$('#second-slider').slider({minValue: 3, maxValue: 19, step: 5, value: 8, scale: true, configuration: true});
 $('#third-slider').slider({minValue: 2, maxValue: 31, step: 8, direction: 'vertical', scale: true, configuration: true});
 $('#fourth-slider').slider({type: 'interval', value: [10, 31], direction: 'vertical', scale: true, step: 5, configuration: true, hint: false});
