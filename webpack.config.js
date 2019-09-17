2292c8e75c18e9b06a4c4f008cf7f33140c79857
Kate Pronina <kate-pronina-97@yandex.ru>
2019-09-17 21:18:07 +0300

    Переписала демо-страницу с помощью pug #4

--- webpack.config.js
+++ webpack.config.js
@@ -1,68 +1,79 @@
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 const webpack = require('webpack');
 
 let conf = {
-    entry: './src/app.ts',
-    output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: 'bundle.js',
-        publicPath: 'dist/'
-    },
-    devServer: {
-        overlay: true
-    },
-    module: {
-        rules: [
+  entry: './src/app.ts',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    overlay: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        loaders: 'ts-loader'
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.hbs/,
+        loader: 'handlebars-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.pug$/,
+        loader: 'pug-loader',
+        options: {
+          pretty: true
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
             {
-                test: /\.ts$/,
-                exclude: /node_modules/,
-                loaders: 'ts-loader'
+              loader: 'css-loader', 
+              options: { sourceMap: true }
             },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader'
-            },
-            {
-                test: /\.hbs/,
-                loader: 'handlebars-loader',
-                exclude: /node_modules/
-            },
-            {
-                test: /\.scss$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: [
-                      {
-                        loader: 'css-loader', 
-                        options: { sourceMap: true }
-                      },
-                      { 
-                        loader: 'sass-loader', 
-                        options: { sourceMap: true }
-                      }
-                    ]
-                })
+            { 
+              loader: 'sass-loader', 
+              options: { sourceMap: true }
             }
-        ]
-    },
-    resolve: {
-        extensions: ['.ts', '.js', '.json']
-    },
-    plugins: [
-        new ExtractTextPlugin('style.css'),
-        new webpack.ProvidePlugin({
-            $: 'jquery',
-            jQuery: 'jquery'
+          ]
         })
+      }
     ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.json']
+  },
+  plugins: [
+    new ExtractTextPlugin('style.css'),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/demo-page/index.pug'
+    }),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery'
+    })
+  ]
 };
 
 module.exports = (env, options) => {
-    conf.devtool = options.mode === "production" ? 
-                    false :
-                    "inline-source-map";
+  conf.devtool = options.mode === "production" ? 
+                  false :
+                  "inline-source-map";
 
-    return conf;
+  return conf;
 };
